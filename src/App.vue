927cb22a1cc6f81060a52d511d2dc9a06d4439b3
Anatoly Melnik <tolyamelnik020406@gmail.com>
2023-05-17 23:07:21 +0800

    da

--- src/App.vue
+++ src/App.vue
@@ -5,119 +5,202 @@ export default {
       leave_data: [],
       demand_data: [],
       price: [],
-      number_clients: '',
+      number_clients: "",
       advertising: false,
       development: false,
-      site_traffic: '',
-      brand_requests: '',
+      site_traffic: "",
+      brand_requests: "",
       partners: [],
       strategies: {
-        integration: false,
-        growth: false,
+        integration: {
+          value: false,
+          name: "уход с рынка конкурентов И большая клиентская база И развитие информационных технологий в отрасли ИЛИ ценовая конкуренция, значит Интеграция = истина",
+        },
+        growth: {
+          value: false,
+          name: "известное на рынке имя И появление новых партнеров И развитие информационных технологий в отрасли ИЛИ большая клиентская база, значит концентрированный рост = истина",
+        },
       },
       conditions: {
-        leave: false,
-        large_client_base: false,
-        competition: false,
-        popularity: false,
-        new_partners: false
-      }
-    }
+        leave: {
+          value: false,
+          name: "количество конкурентов предыдущего месяца меньше текущего количества конкурентов на 20%, значит уход с рынка конкурентов = истина",
+          rule: () => (this.leave_data[0] / this.leave_data[1]) * 100 <= 80,
+        },
+        large_client_base: {
+          value: false,
+          name: "количество клиентов больше 3000, значит большая клиентская база = истина",
+          rule: () => this.number_clients > 3000,
+        },
+        competition: {
+          value: false,
+          name: "конкурент обширно рекламировал товар по низким ценам И спрос повысился на 30% И цена товара выросла на 30%, значит ценовая конкуренция = истина",
+          rule: () =>
+            this.advertising &&
+            (this.demand_data[1] / this.demand_data[0]) * 100 >= 130 &&
+            (this.price[1] / this.price[0]) * 100 >= 130,
+        },
+        popularity: {
+          value: false,
+          name: "посещаемость сайта в сутки > 1000 человек) И (запросы в интернете связанные с брендом в месяц > 7000, значит известное на рынке имя = истина",
+          rule: () => this.site_traffic > 1000 && this.brand_requests > 7000,
+        },
+        new_partners: {
+          value: false,
+          name: "количество партнеров увеличилось на 20%, значит появление новых партнеров = истина",
+          rule: () => (this.partners[1] / this.partners[0]) * 100 >= 120,
+        },
+      },
+    };
   },
   methods: {
     calculate() {
       {
-        // Сравниваем базу фактов с базой знаний
-        (this.leave_data[0]/this.leave_data[1])*100 <= 80 ? this.conditions.leave = true : this.conditions.leave = false;
-        this.number_clients > 3000 ? this.conditions.large_client_base = true : this.conditions.large_client_base = false;
-        this.advertising && ((this.demand_data[1]/this.demand_data[0])*100 >= 130) && ((this.price[1]/this.price[0])*100 >= 130) ? this.conditions.competition = true : this.conditions.competition = false;
-        this.site_traffic > 1000 && this.brand_requests > 7000 ? this.conditions.popularity = true : this.conditions.popularity = true
-        (this.partners[1]/this.partners[0])*100 >= 120 ? this.conditions.new_partners = true : this.conditions.new_partners = false
-
         // Выбор стратегии
-        this.conditions.leave && this.conditions.large_client_base && (this.conditions.development || this.conditions.competition) ? this.strategies.integration = true : this.strategies.integration = false
-        this.conditions.popularity && this.conditions.new_partners && (this.conditions.development || this.conditions.large_client_base) ? this.strategies.growth = true : this.strategies.growth = false
+
+        this.conditions.forEach((condition) => {
+          this.conditions.leave.value &&
+          this.conditions.large_client_base.value &&
+          (this.conditions.development.value ||
+            this.conditions.competition.value)
+            ? (this.strategies.integration.value = true)
+            : (this.strategies.integration.value = false);
+          this.conditions.popularity.value &&
+          this.conditions.new_partners.value &&
+          (this.conditions.development.value ||
+            this.conditions.large_client_base.value)
+            ? (this.strategies.growth.value = true)
+            : (this.strategies.growth.value = false);
+
+          if (condition.rule()) {
+            console.log("YES YES YES");
+          }
+        });
+        // Сравниваем базу фактов с базой знаний
+        // ( ? this.conditions.leave.value = true : this.conditions.leave.value = false;
+        //  ? this.conditions.large_client_base.value = true : this.conditions.large_client_base.value = false;
+        //  ? this.conditions.competition.value = true : this.conditions.competition.value = false;
+        //  ? this.conditions.popularity.value = true : this.conditions.popularity.value = true
+        //  ? this.conditions.new_partners.value = true : this.conditions.new_partners.value = false
       }
     },
     checkKey(evt) {
-      const key = evt.key
-      const keysAllowed = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+      const key = evt.key;
+      const keysAllowed = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
       if (!keysAllowed.includes(key)) {
         evt.preventDefault();
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <template>
   <form @submit.prevent>
-      <table>
-        <tr>
-          <th></th>
-          <th>Текущий месяц</th>
-          <th>Предыдущий месяц</th>
-        </tr>
-        <tr>
-          <td>Кол-во конкурентов</td>
-          <td>
-            <input type="text" @keypress="checkKey($event)" v-model="leave_data[1]" />
-          </td>
-          <td>
-            <input type="text" @keypress="checkKey($event)" v-model="leave_data[0]" />
-          </td>
-        </tr>
-        <tr>
-          <td>Количество клиентов</td>
-          <td>
-            <input type="text" @keypress="checkKey($event)" v-model="number_clients" />
-          </td>
-        </tr>
-        <tr>
-          <td>Количество заказов</td>
-          <td>        
-            <input type="text" @keypress="checkKey($event)" v-model="demand_data[1]" />
-          </td>
-          <td>
-            <input type="text" @keypress="checkKey($event)" v-model="demand_data[0]" />
-          </td>
-        </tr>
-        <tr>
-          <td>Цена товара</td>
-          <td>        
-            <input type="text" @keypress="checkKey($event)" v-model="price[1]" />
-          </td>
-          <td>
-            <input type="text" @keypress="checkKey($event)" v-model="price[0]" />
-          </td>
-        </tr>
-        <tr>
-          <td>Количество запросов, связанных с брендом</td>
-          <td>
-            <input type="text" @keypress="checkKey($event)" v-model="brand_requests" />
-          </td>
-        </tr>
-        <tr>
-          <td>Количество партнеров</td>
-          <td>        
-            <input type="text" @keypress="checkKey($event)" v-model="partners[1]" />
-          </td>
-          <td>
-            <input type="text" @keypress="checkKey($event)" v-model="partners[0]" />
-          </td>
-        </tr>
-        <tr>
-          <td>Посещаемость сайта</td>
-          <td>        
-            <input type="text" @keypress="checkKey($event)" v-model="site_traffic" />
-          </td>
-        </tr>
-      </table>
+    <table>
+      <tr>
+        <th></th>
+        <th>Текущий месяц</th>
+        <th>Предыдущий месяц</th>
+      </tr>
+      <tr>
+        <td>Кол-во конкурентов</td>
+        <td>
+          <input
+            type="text"
+            @keypress="checkKey($event)"
+            v-model="leave_data[1]"
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            @keypress="checkKey($event)"
+            v-model="leave_data[0]"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td>Количество клиентов</td>
+        <td>
+          <input
+            type="text"
+            @keypress="checkKey($event)"
+            v-model="number_clients"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td>Количество заказов</td>
+        <td>
+          <input
+            type="text"
+            @keypress="checkKey($event)"
+            v-model="demand_data[1]"
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            @keypress="checkKey($event)"
+            v-model="demand_data[0]"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td>Цена товара</td>
+        <td>
+          <input type="text" @keypress="checkKey($event)" v-model="price[1]" />
+        </td>
+        <td>
+          <input type="text" @keypress="checkKey($event)" v-model="price[0]" />
+        </td>
+      </tr>
+      <tr>
+        <td>Количество запросов, связанных с брендом</td>
+        <td>
+          <input
+            type="text"
+            @keypress="checkKey($event)"
+            v-model="brand_requests"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td>Количество партнеров</td>
+        <td>
+          <input
+            type="text"
+            @keypress="checkKey($event)"
+            v-model="partners[1]"
+          />
+        </td>
+        <td>
+          <input
+            type="text"
+            @keypress="checkKey($event)"
+            v-model="partners[0]"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td>Посещаемость сайта</td>
+        <td>
+          <input
+            type="text"
+            @keypress="checkKey($event)"
+            v-model="site_traffic"
+          />
+        </td>
+      </tr>
+    </table>
     <div class="second">
       <div>
         <input type="checkbox" v-model="advertising" id="c1" />
-        <label for="c1">Конкурент обширно рекламировал товар по низким ценам</label>
+        <label for="c1"
+          >Конкурент обширно рекламировал товар по низким ценам</label
+        >
       </div>
       <div>
         <input type="checkbox" v-model="development" id="c2" />
@@ -128,20 +211,21 @@ export default {
   </form>
   <div>
     <div v-if="strategies.integration">Вам подходит стратегия интеграции</div>
-    <div v-if="strategies.growth">Вам подходит стратегия концентрированного роста</div>
+    <div v-if="strategies.growth">
+      Вам подходит стратегия концентрированного роста
+    </div>
   </div>
 </template>
 
 <style>
 form {
   display: grid;
-  gap : 9px 20px;
+  gap: 9px 20px;
   width: 900px;
   justify-content: center;
   align-items: center;
-  grid-template-columns:  2fr 1fr;
+  grid-template-columns: 2fr 1fr;
 }
-
 
 /* form > div > div {
   text-align: start;
@@ -152,7 +236,7 @@ form {
   text-align: start;
 }
 
-input[type='number'] {
+input[type="number"] {
   display: block;
 }
 
@@ -161,7 +245,6 @@ form > div > div > span {
   position: relative;
   top: -2px;
 }
-
 
 label {
   user-select: none;
